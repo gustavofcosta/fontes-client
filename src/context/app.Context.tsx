b5dd2638b88acc8ffca60c040f3b2bfb1750760b
gustavofcosta <gfcdesenvolvimento@gmail.com>
2022-11-24 16:14:14 -0300

    config appContext

---
+++ src/context/app.Context.tsx
@@ -0,0 +1,52 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+import axios from "../services/axios";
+
+interface ChildrenProps {
+  children: ReactNode;
+}
+
+interface InitialContextInterface {
+  handleRegister: () => Promise<void>;
+  loading: boolean;
+}
+
+export const AppContext = createContext<InitialContextInterface>(
+  {} as InitialContextInterface
+);
+
+export const AppProvider = ({ children }: ChildrenProps) => {
+  const [loading, setLoading] = useState(false);
+  const [token, setToken] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const addTokenToLocalStorage = () => {
+    localStorage.setItem("token", token);
+  };
+
+  const removeTokenToLocalStorage = () => {
+    localStorage.removeItem("token");
+  };
+
+  const handleRegister = async () => {
+    setLoading(true);
+    try {
+      let { data } = await axios.post("/login", { username, password });
+      setToken(data.access_token);
+      setLoading(false);
+      addTokenToLocalStorage();
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <AppContext.Provider value={{ handleRegister, loading }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
